f59cf2be1b92a2fae3864312d25616b1d28f4d34
Giedrius Mikoliūnas <giedrius@lympo.io>
2020-01-12 11:32:12 +0200

    create RNPromise

--- ios/RNTapdaq.m
+++ ios/RNTapdaq.m
@@ -1,13 +1,16 @@
 #import "RNTapdaq.h"
+#import <Tapdaq/Tapdaq.h>
 
 @implementation RNTapdaq
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
-{
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+RCT_EXPORT_METHOD(initialize:(NSString *)applicationId
+                  clientKey:(NSString *)clientKey
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    TDProperties *tdProperties = [[TDProperties alloc] init];
+    [Tapdaq.sharedSession setApplicationId:applicationId clientKey:clientKey properties:tdProperties];
 }
 
 @end
